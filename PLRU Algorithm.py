0295009da49aed22f345ba811dff1aa7c1f6c2c4
wasey299 <117496015+wasey299@users.noreply.github.com>
2022-12-09 11:51:26 -0800

    Update PLRU Algorithm.py

--- PLRU Algorithm.py
+++ PLRU Algorithm.py
@@ -76,39 +76,44 @@ print([cache_line])
 
 # ---Eviction Using PLRU policy------------------------------------------------------------------
 # ---Complimenting PLRU Bits---------------------------------------------------------------------
-cache_line[17] = Compliment(cache_line[17])
-cache_line[18] = Compliment(cache_line[18])
-cache_line[19] = Compliment(cache_line[19])
-cache_line[20] = Compliment(cache_line[20])
-cache_line[21] = Compliment(cache_line[21])
-cache_line[22] = Compliment(cache_line[22])
-cache_line[23] = Compliment(cache_line[23])
-
-# ---Eviction to add 2 by the means of the PLRU branch------------------------------------------------------------------------------------
 if cache_line[17] == 0:
-    if cache_line[18] == 0:
-        if cache_line[20] == 0:
-            cache_line[2] = 2
-        elif cache_line[20] == 1:
-            cache_line[4] = 2
-    elif cache_line[18] == 1:
-        if cache_line[21] == 0:
-            cache_line[6] = 2
-        elif cache_line[21] == 1:
-            cache_line[8] = 2
+    cache_line[17] = 1
+    if cache_line[19] == 0:
+        cache_line[19] = 1
+        if cache_line[23] == 0:
+            cache_line[23] = 1
+            cache_line[16] = 2
+        elif cache_line[23] == 1:
+            cache_line[23] = 0
+            cache_line[14] = 2
+    elif cache_line[19] == 1:
+        cache_line[19] = 0
+        if cache_line[22] == 0:
+            cache_line[22] = 1
+            cache_line[12] = 2
+        elif cache_line[22] == 1:
+            cache_line[22] = 0
+            cache_line[10] = 2
+
 
 elif cache_line[17] == 1:
-    if cache_line[19] == 0:
-        if cache_line[22] == 0:
-            cache_line[10] = 2
-        if cache_line[22] == 1:
-            cache_line[12] = 2
-    elif cache_line[19] == 1:
-        if cache_line[23] == 0:
-            cache_line[14] = 2
-        elif cache_line[23] == 1:
-            cache_line[16] == 2
+    cache_line[17] = 0
+    if cache_line[18] == 0:
+        cache_line[18] = 1
+        if cache_line[21] == 0:
+            cache_line[21] = 1
+            cache_line[8] = 2
+        elif cache_line[21] == 1:
+            cache_line[21] = 0
+            cache_line[6] = 2
+    elif cache_line[18] == 1:
+        cache_line[18] = 0
+        if cache_line[20] == 0:
+            cache_line[20] = 1
+            cache_line[4] = 2
+        elif cache_line[20] == 1:
+            cache_line[20] = 0
+            cache_line[2] = 2
 
 
 print([cache_line])
-
